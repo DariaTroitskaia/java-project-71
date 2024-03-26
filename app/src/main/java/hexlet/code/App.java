b3180f5bd742b0b1ef848559fa5dd84ba82e24b3
DariaTroitskaia <troizkaja2011@gmail.com>
2024-03-26 20:23:30 +0300

    add option format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,22 @@
 package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true)
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable{
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
     @Override
     public void run() {
     }
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
