7982cca42e6e58b40004d32df17ffe8d1a5d772b
DariaTroitskaia <troizkaja2011@gmail.com>
2024-03-26 19:31:07 +0300

    add util --help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,15 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
-public class App {
+@Command(name = "gendiff", mixinStandardHelpOptions = true)
+public class App implements Runnable{
+    @Override
+    public void run() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
