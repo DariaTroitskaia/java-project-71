fab1d00217bb660742e19f7db59bfc622219d464
DariaTroitskaia <troizkaja2011@gmail.com>
2024-05-01 16:48:44 +0300

    add checkstyle and github-actions

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,7 +4,6 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,7 +11,7 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable{
+public class App implements Callable {
 
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
@@ -21,7 +20,7 @@ public class App implements Callable{
     @Option(names = {"-f", "--format"}, paramLabel = "FORMAT", description = "output format [default: stylish]")
     String format;
     @Override
-    public Object call() throws Exception{
+    public Object call() throws Exception {
         try {
             System.out.println(Differ.generate(filepath1.toString(), filepath2.toString()));
             return null;
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,7 +2,8 @@ package hexlet.code;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.TreeSet;
+import java.util.TreeMap;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,23 +15,23 @@ public class Differ {
         Path path2 = Paths.get(file2).toAbsolutePath().normalize();
 
         ObjectMapper mapper =  new ObjectMapper();
-        TreeMap<String, Object> mapFile1 = mapper.readValue(Files.readString(path1), new TypeReference<>(){});
-        TreeMap<String, Object> mapFile2 = mapper.readValue(Files.readString(path2), new TypeReference<>(){});
+        TreeMap<String, Object> mapFile1 = mapper.readValue(Files.readString(path1), new TypeReference<>() { });
+        TreeMap<String, Object> mapFile2 = mapper.readValue(Files.readString(path2), new TypeReference<>() { });
 
         var keys = new TreeSet(mapFile1.keySet());
         keys.addAll(mapFile2.keySet());
 
         String result = "{\n";
-        for(var key : keys) {
+        for (var key : keys) {
             if (mapFile1.containsKey(key) && mapFile2.containsKey(key)) {
                 String value1 = mapFile1.get(key).toString();
                 String value2 = mapFile2.get(key).toString();
-                if(value1.equals(value2)) {
+                if (value1.equals(value2)) {
                     result += "    " + key + ": " + value1 + "\n";
                 } else {
                     result += "  - " + key + ": " + value1 + "\n  + " + key + ": " + value2 + "\n";
                 }
-            } else if(mapFile1.containsKey(key) && !mapFile2.containsKey(key)) {
+            } else if (mapFile1.containsKey(key) && !mapFile2.containsKey(key)) {
                 String value1 = mapFile1.get(key).toString();
                 result += "  - " + key + ": " + value1 + "\n";
             } else {
